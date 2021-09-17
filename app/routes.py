c7c35d1b53954bf533c978952dfd8a9e45eba818
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-09-17 14:01:04 +0800

    Get routes to work

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/students')
+def students():
+    return render_template('students.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/colleges')
+def colleges():
+    return render_template('colleges.html')
