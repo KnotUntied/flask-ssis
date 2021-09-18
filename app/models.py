7ce455b343cc256cbc71b2f6ff49e8056201446f
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-09-18 18:43:04 +0800

    Test SQLite via Flask-SQLAlchemy and Flask-Migrate

---
+++ app/models.py
@@ -0,0 +1,34 @@
+import enum
+
+from app import db
+
+class Gender(enum.Enum):
+    # https://docs.sqlalchemy.org/en/14/core/type_basics.html#sqlalchemy.types.Enum
+    # Enum names are persisted, not values.
+    Other = 1
+    Male = 2
+    Female = 3
+
+class Student(db.Model):
+    id = db.Column(db.CHAR(9), primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    course = db.Column(db.String(10), db.ForeignKey('course.code'), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    # https://stackoverflow.com/questions/44343431/flask-sqlalchemy-enum-field-default-value
+    gender = db.Column(db.Enum(Gender), nullable=False, server_default='Other')
+
+    # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
+    # The __repr__ method tells Python how to print objects of this class
+    def __repr__(self):
+        return '<Student {}>'.format(self.id)
+
+class Course(db.Model):
+    code = db.Column(db.String(10), primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    college = db.Column(db.String(5), nullable=False)
+    # backref cannot be the same as a key in the other table :(
+    students = db.relationship('Student', backref='course_code', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Course {}>'.format(self.code)
--- config.py
+++ config.py
@@ -1,10 +1,16 @@
-from os import getenv
+from os import getenv, path
+
+basedir = path.abspath(path.dirname(__file__))
 
 class Config(object):
     # Used for securely signing the session cookie and can be used for other security-related needs
-    SECRET_KEY = getenv("SECRET_KEY")
+    SECRET_KEY = getenv("SECRET_KEY") or 'you-will-never-guess'
     DB_NAME = getenv("DB_NAME")
     DB_USERNAME = getenv("DB_USERNAME")
     DB_PASSWORD = getenv("DB_PASSWORD")
     DB_HOST = getenv("DB_HOST")
     BOOTSTRAP_SERVE_LOCAL = getenv("BOOTSTRAP_SERVE_LOCAL")
+
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL') or \
+        'sqlite:///' + path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
