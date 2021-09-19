faf286f25db2d3093aa33403f88b2e2687242cdb
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-09-19 20:33:02 +0800

    Update models

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField, StringField, SubmitField
 from wtforms.validators import ValidationError, AnyOf, DataRequired, Length, NumberRange, Regexp
-from app.models import Student, Course
+from app.models import Student, Course, College
 
 def validate_name(form, field):
     excluded_chars = '1234567890'
@@ -11,18 +11,10 @@ def validate_name(form, field):
                 f'Character {char} is not allowed in name.')
 
 class StudentForm(FlaskForm):
-    # idyear = IntegerField(validators=[DataRequired()])
-    # idnumber = IntegerField(validators=[DataRequired()])
     id = StringField(label='ID Number (YYYY-NNNN)', validators=[DataRequired(), Regexp(regex=r'\d{4}\-\d{4}$')])
     firstname = StringField(label='First Name', validators=[DataRequired(), Length(min=1, max=50), validate_name])
     lastname = StringField(label='Last Name', validators=[DataRequired(), Length(min=1, max=50), validate_name])
-    # TODO: Get course list
-    course = SelectField(label='Course',
-        choices=['test1', 'test2', 'test3'],
-        validators=[
-            DataRequired(),
-            AnyOf(values=['test1', 'test2', 'test3'])
-        ])
+    course = SelectField(label='Course', validators=[DataRequired()])
     year = IntegerField(label='Year Level', validators=[DataRequired(), NumberRange(min=1, max=4)])
     gender = SelectField(label='Gender',
         choices=['Other', 'Male', 'Female'],
@@ -44,13 +36,39 @@ class EditStudentForm(StudentForm):
         if new_id is not None and new_id == id:
             raise ValidationError('ID number has already been used.')
 
+
 class CourseForm(FlaskForm):
     code = StringField(label='Course Code', validators=[DataRequired(), Length(min=1, max=10)])
     name = StringField(label='Course Name', validators=[DataRequired(), Length(min=1, max=50)])
-    college = StringField(label='College', validators=[DataRequired(), AnyOf(values=['test1', 'test2', 'test3'])])
+    college = StringField(label='College', validators=[DataRequired()])
     submit = SubmitField(label='Submit')
 
+class AddCourseForm(CourseForm):
+    def validate_code(self, code):
+        code = Course.query.filter_by(code=code.data).first()
+        if code is not None:
+            raise ValidationError('Course code has already been used.')
+
+class EditCourseForm(CourseForm):
+    def validate_code(self, code):
+        new_code = Course.query.filter_by(code=code.data).first()
+        if new_code is not None and new_code == code:
+            raise ValidationError('Course code has already been used.')
+
+
 class CollegeForm(FlaskForm):
-    code = StringField(label='College Code', validators=[DataRequired()])
-    name = StringField(label='College Name', validators=[DataRequired()])
+    code = StringField(label='College Code', validators=[DataRequired(), Length(min=1, max=5)])
+    name = StringField(label='College Name', validators=[DataRequired(), Length(min=1, max=50)])
     submit = SubmitField(label='Submit')
+
+class AddCollegeForm(CollegeForm):
+    def validate_code(self, code):
+        code = College.query.filter_by(code=code.data).first()
+        if code is not None:
+            raise ValidationError('College code has already been used.')
+
+class EditCollegeForm(CollegeForm):
+    def validate_code(self, code):
+        new_code = College.query.filter_by(code=code.data).first()
+        if new_code is not None and new_code == code:
+            raise ValidationError('College code has already been used.')
--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class Student(db.Model):
     id = db.Column(db.CHAR(9), primary_key=True)
     firstname = db.Column(db.String(50), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
-    course = db.Column(db.String(10), db.ForeignKey('course.code'), nullable=False)
+    course = db.Column(db.String(10), db.ForeignKey('course.code', onupdate='CASCADE', ondelete='SET NULL'))
     year = db.Column(db.Integer, nullable=False)
     # https://stackoverflow.com/questions/44343431/flask-sqlalchemy-enum-field-default-value
     # Enums in SQLAlchemy carry the enum when queried, bringing some clunk in the process.
@@ -28,9 +28,20 @@ class Student(db.Model):
 class Course(db.Model):
     code = db.Column(db.String(10), primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    college = db.Column(db.String(5), nullable=False)
-    # backref cannot be the same as a key in the other table :(
-    students = db.relationship('Student', backref='course_code', lazy='dynamic')
+    college = db.Column(db.String(5), db.ForeignKey('college.code', onupdate='CASCADE', ondelete='SET NULL'))
 
     def __repr__(self):
         return '<Course {}>'.format(self.code)
+
+    def to_choice(self):
+        return (self.code, '{} ({})'.format(self.name, self.code))
+
+class College(db.Model):
+    code = db.Column(db.String(5), primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<College {}>'.format(self.code)
+
+    def to_choice(self):
+        return (self.code, '{} ({})'.format(self.name, self.code))
