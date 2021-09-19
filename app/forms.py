4fc2e1a33d87649e4f07a17c99f7a662abe94312
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-09-19 15:03:05 +0800

    Add "edit" student functionality

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField, StringField, SubmitField
-from wtforms.validators import ValidationError, AnyOf, DataRequired, NumberRange, Regexp
+from wtforms.validators import ValidationError, AnyOf, DataRequired, Length, NumberRange, Regexp
 from app.models import Student, Course
 
 def validate_name(form, field):
@@ -14,8 +14,8 @@ class StudentForm(FlaskForm):
     # idyear = IntegerField(validators=[DataRequired()])
     # idnumber = IntegerField(validators=[DataRequired()])
     id = StringField(label='ID Number (YYYY-NNNN)', validators=[DataRequired(), Regexp(regex=r'\d{4}\-\d{4}$')])
-    firstname = StringField(label='First Name', validators=[DataRequired(), validate_name])
-    lastname = StringField(label='Last Name', validators=[DataRequired(), validate_name])
+    firstname = StringField(label='First Name', validators=[DataRequired(), Length(min=1, max=50), validate_name])
+    lastname = StringField(label='Last Name', validators=[DataRequired(), Length(min=1, max=50), validate_name])
     # TODO: Get course list
     course = SelectField(label='Course',
         choices=['test1', 'test2', 'test3'],
@@ -32,14 +32,21 @@ class StudentForm(FlaskForm):
         ])
     submit = SubmitField(label='Submit')
 
+class AddStudentForm(StudentForm):
     def validate_id(self, id):
         id = Student.query.filter_by(id=id.data).first()
         if id is not None:
             raise ValidationError('ID number has already been used.')
 
+class EditStudentForm(StudentForm):
+    def validate_id(self, id):
+        new_id = Student.query.filter_by(id=id.data).first()
+        if new_id is not None and new_id == id:
+            raise ValidationError('ID number has already been used.')
+
 class CourseForm(FlaskForm):
-    code = StringField(label='Course Code', validators=[DataRequired()])
-    name = StringField(label='Course Name', validators=[DataRequired()])
+    code = StringField(label='Course Code', validators=[DataRequired(), Length(min=1, max=10)])
+    name = StringField(label='Course Name', validators=[DataRequired(), Length(min=1, max=50)])
     college = StringField(label='College', validators=[DataRequired(), AnyOf(values=['test1', 'test2', 'test3'])])
     submit = SubmitField(label='Submit')
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, request, url_for
 
 from app import app, db
-from app.forms import StudentForm
+from app.forms import AddStudentForm, EditStudentForm
 from app.models import Student, Course
 
 @app.route('/')
@@ -13,7 +13,7 @@ def students():
 
 @app.route('/students/add/', methods=['GET', 'POST'])
 def add_student():
-    form = StudentForm()
+    form = AddStudentForm()
     if form.validate_on_submit():
         student = Student(
             id=form.id.data,
@@ -22,7 +22,6 @@ def add_student():
             course=form.course.data,
             year=form.year.data,
             gender=form.gender.data)
-        print(form.gender.data)
         db.session.add(student)
         db.session.commit()
         flash('{} {} {} has been added.'.format(form.id.data, form.firstname.data, form.lastname.data))
@@ -34,6 +33,29 @@ def add_student():
 def view_student(id):
     student = Student.query.filter_by(id=id).first_or_404()
     return render_template('student_view.html', student=student)
+
+@app.route('/students/<id>/edit', methods=['GET', 'POST'])
+def edit_student(id):
+    student = Student.query.get(id)
+    form = EditStudentForm()
+    if form.validate_on_submit():
+        student.id        = form.id.data
+        student.firstname = form.firstname.data
+        student.lastname  = form.lastname.data
+        student.course    = form.course.data
+        student.year      = form.year.data
+        student.gender    = form.gender.data
+        db.session.commit()
+        flash('Edit successful.')
+        return redirect(url_for('view_student', id=student.id))
+    elif request.method == 'GET':
+        form.id.data        = student.id
+        form.firstname.data = student.firstname
+        form.lastname.data  = student.lastname
+        form.course.data    = student.course
+        form.year.data      = student.year
+        form.gender.data    = student.gender
+    return render_template('student_form.html', title='Edit Student', form=form)
 
 @app.route('/courses/')
 def courses():
