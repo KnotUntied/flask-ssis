132060f1a49459d7baa829378e224b41ee202a1d
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-09-19 20:37:47 +0800

    Update routes

--- app/routes.py
+++ app/routes.py
@@ -7,13 +7,18 @@ from app.models import Student, Course
 @app.route('/')
 @app.route('/index/')
 @app.route('/students/')
-def students():
+def students(field=None, text=None):
     students = Student.query.all()
     return render_template('students.html', title='Students', students=students)
 
 @app.route('/students/add/', methods=['GET', 'POST'])
 def add_student():
+    # TODO: Instruct user to create course if no courses created.
     form = AddStudentForm()
+
+    courses = Course.query.all()
+    form.course.choices = list(course.to_choice() for course in courses)
+
     if form.validate_on_submit():
         student = Student(
             id=form.id.data,
@@ -28,16 +33,21 @@ def add_student():
         return redirect(url_for('students'))
     return render_template('student_form.html', title='Add Student', form=form)
 
-# Redundant as of now, but may be expanded in the future
+# Unnecessary as of now, but may be expanded in the future.
 @app.route('/students/<id>')
 def view_student(id):
     student = Student.query.filter_by(id=id).first_or_404()
-    return render_template('student_view.html', student=student)
+    course = Course.query.get(student.course)
+    return render_template('student_view.html', student=student, course=course)
 
 @app.route('/students/<id>/edit', methods=['GET', 'POST'])
 def edit_student(id):
     student = Student.query.get(id)
     form = EditStudentForm()
+
+    courses = Course.query.all()
+    form.course.choices = list(course.to_choice() for course in courses)
+
     if form.validate_on_submit():
         student.id        = form.id.data
         student.firstname = form.firstname.data
@@ -49,10 +59,16 @@ def edit_student(id):
         flash('Edit successful.')
         return redirect(url_for('view_student', id=student.id))
     elif request.method == 'GET':
+
         form.id.data        = student.id
         form.firstname.data = student.firstname
         form.lastname.data  = student.lastname
-        form.course.data    = student.course
+        if student.course in [choice[0] for choice in form.course.choices]:
+            form.course.data = student.course
+        # TODO: Instruct user to create course if no courses created.
+        elif form.course.choices:
+            form.course.data = form.course.choices[0][0]
+        print(form.course.data)
         form.year.data      = student.year
         form.gender.data    = student.gender
     return render_template('student_form.html', title='Edit Student', form=form)
