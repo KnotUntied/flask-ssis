a4d0ecbfa946c140fcf12e155e379c5e519f2c1c
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-09-20 14:57:16 +0800

    Use blueprint from error handling

--- app/__init__.py
+++ app/__init__.py
@@ -12,4 +12,7 @@ bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models, errors
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app import routes, models
