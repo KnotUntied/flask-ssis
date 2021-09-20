7801c5833e1dfdbf81139700ab788d8fa624b2f5
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-09-20 13:56:48 +0800

    Use Flask-Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
-from config import Config
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from config import Config
+
 app = Flask(__name__)
 app.config.from_object(Config)
+
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
