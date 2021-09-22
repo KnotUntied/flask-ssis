f4c9df146ca22cd2ff2f5a03826c70b62b41ff73
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-09-22 12:49:45 +0800

    Search functionality progress (no lists in arguments yet, no query filters yet)

--- app/students/forms.py
+++ app/students/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SelectField, StringField, SubmitField
+from wtforms import IntegerField, SelectField, SelectMultipleField, StringField, SubmitField
 from wtforms.validators import ValidationError, AnyOf, DataRequired, Length, NumberRange, Regexp
+from wtforms.widgets import CheckboxInput
 from app.models import Student
+from app.main.forms import MultiCheckboxField
 
+
+years = [1, 2, 3, 4]
+genders = ['Other', 'Male', 'Female']
 
 def validate_name(form, field):
     excluded_chars = '1234567890'
@@ -17,12 +22,10 @@ class StudentForm(FlaskForm):
     lastname = StringField(label='Last Name', validators=[DataRequired(), Length(min=1, max=50), validate_name])
     course = SelectField(label='Course', validators=[DataRequired()])
     year = IntegerField(label='Year Level', validators=[DataRequired(), NumberRange(min=1, max=4)])
-    gender = SelectField(label='Gender',
-        choices=['Other', 'Male', 'Female'],
-        validators=[
-            DataRequired(),
-            AnyOf(values=['Other', 'Male', 'Female'])
-        ])
+    gender = SelectField(
+        label='Gender',
+        choices=genders,
+        validators=[DataRequired(), AnyOf(values=genders)])
     submit = SubmitField(label='Submit')
 
 class AddStudentForm(StudentForm):
@@ -36,3 +39,19 @@ class EditStudentForm(StudentForm):
         new_id = Student.query.filter_by(id=id.data).first()
         if new_id is not None and new_id == id:
             raise ValidationError('ID number has already been used.')
+
+class SearchStudentForm(FlaskForm):
+    id = StringField(label='ID Number (YYYY-NNNN)')
+    firstname = StringField(label='First Name', validators=[Length(max=50), validate_name])
+    lastname = StringField(label='Last Name', validators=[Length(max=50), validate_name])
+    course = SelectMultipleField(label='Course (can select multiple)')
+    year = MultiCheckboxField(
+        label='Year Level',
+        # 2-tuples are required for SelectMultipleField, for some reason
+        choices=[(y, str(y)) for y in years],
+        option_widget=CheckboxInput())
+    gender = MultiCheckboxField(
+        label='Gender',
+        choices=[(g, g) for g in genders],
+        option_widget=CheckboxInput())
+    submit = SubmitField(label='Submit')
