68ea58d0deb0b7355144a675322e06a8426044d2
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-09-24 08:45:36 +0800

    Add CLI commands

--- app/cli.py
+++ app/cli.py
@@ -6,7 +6,16 @@ from flask import current_app
 from app import db
 
 def register(app):
-    @app.cli.command("create-db")
-    def create_db():
+    @app.cli.command('init-db')
+    def init_db():
         cursor = db.connection.cursor()
-        # cursor.execute("CREATE DATABASE ssis")
+        with open('init.sql') as f:
+            read_data = f.read()
+            cursor.executescript(read_data)
+
+    @app.cli.command('sample')
+    def populate_sample():
+        cursor = db.connection.cursor()
+        with open('sample.sql') as f:
+            read_data = f.read()
+            cursor.executescript(read_data)
